072c57faef626e6df639c1219dfe1d06904e27bd
Cebajel Tanan <cebajel@gmail.com>
2023-09-15 19:06:22 +0530

    Done

--- src/matmul.cpp
+++ src/matmul.cpp
@@ -3,21 +3,25 @@
 // using -O3, loop interchange, and parallelization optimizations.
 
 #include <iostream>
-#include<cmath>
+#include <cmath>
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
 #include <immintrin.h>
 #ifdef BLAS
-#include<cblas.h>
+#include <cblas.h>
 #endif
 
 #ifdef STACKALLOCATED
 #define INPUTSIZE 256
 #endif
 
+/*
+    This function is used to compare two matrices element-wise and returns 1 if the maximum absolute difference between
+    corresponding elements is less than 1E-6, indicating that the matrices are considered similar within the specified tolerance.
+*/
 static int
-verify_result( int n, float *C_ref, float *C)
+verify_result(int n, float *C_ref, float *C)
 {
     float e_max;
     e_max = 0;
@@ -25,7 +29,7 @@ verify_result( int n, float *C_ref, float *C)
     {
         for (int j = 0; j < n; j++)
         {
-            e_max = std::max(std::abs(C[i*n + j] - C_ref[i*n + j]), e_max);
+            e_max = std::max(std::abs(C[i * n + j] - C_ref[i * n + j]), e_max);
         }
     }
 
@@ -33,7 +37,6 @@ verify_result( int n, float *C_ref, float *C)
 
     return e_max < 1E-6;
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -56,33 +59,43 @@ int main(int argc, char *argv[])
         for (int j = 0; j < n; j++)
         {
             A[i][j] = std::rand() / (float)(RAND_MAX);
+            /*
+                If BLAS = 1 then B is stored in Column Major layout
+            */
             #ifdef BLAS
             #if BLAS == 1
-            B[j][i] = std::rand() / (float)(RAND_MAX);
+                B[j][i] = std::rand() / (float)(RAND_MAX);
             #else
-            B[i][j] = std::rand() / (float)(RAND_MAX);
+                B[i][j] = std::rand() / (float)(RAND_MAX);
             #endif
             #else
-            B[i][j] = std::rand() / (float)(RAND_MAX);
+                B[i][j] = std::rand() / (float)(RAND_MAX);
             #endif
             C[i][j] = 0;
         }
     }
 
 #else
+    /*
+        Initializing matrices A, B, C and C_ref 
+        This loop takes layout of matrices into account.
+    */
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
             A[i * n + j] = std::rand() / (float)(RAND_MAX);
+            /*
+                If BLAS = 1 then B is stored in Column Major layout
+            */
             #ifdef BLAS
             #if BLAS == 1
-            B[j * n + i] = std::rand() / (float)(RAND_MAX);
+                B[j * n + i] = std::rand() / (float)(RAND_MAX);
             #else
-            B[i * n + j] = std::rand() / (float)(RAND_MAX);
+                B[i * n + j] = std::rand() / (float)(RAND_MAX);
             #endif
             #else
-            B[i * n + j] = std::rand() / (float)(RAND_MAX);
+                B[i * n + j] = std::rand() / (float)(RAND_MAX);
             #endif
             C[i * n + j] = 0;
             C_ref[i * n + j] = 0;
@@ -93,6 +106,15 @@ int main(int argc, char *argv[])
     const auto start = std::chrono::steady_clock::now();
 #ifdef STACKALLOCATED
 #ifdef LOOPINTERCHANGE
+/*
+    LOOPINTERCHANGE macro is used to dertermine which loop ordering to be used:
+    0 : ijk
+    1 : ikj
+    2 : kij
+    3 : kji
+    4 : jik
+    5 : kji
+*/
 #if LOOPINTERCHANGE == 1
     for (int i = 0; i < n; i++)
         for (int k = 0; k < n; k++)
@@ -118,14 +140,26 @@ int main(int argc, char *argv[])
         for (int j = 0; j < n; j++)
             for (int k = 0; k < n; k++)
 #endif
-        C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][k] * B[k][j];
 #endif
 #else
 
+/*
+    This macro parallelizes the immediate for loop it encounters.
+*/
 #ifdef PARALLEL
 #pragma omp parallel for
 #endif
 
+/*
+    LOOPINTERCHANGE macro is used to dertermine which loop ordering to be used:
+    0 : ijk
+    1 : ikj
+    2 : kij
+    3 : kji
+    4 : jik
+    5 : kji
+*/
 #ifdef LOOPINTERCHANGE
 #if LOOPINTERCHANGE == 1
     for (int i = 0; i < n; i++)
@@ -152,32 +186,66 @@ int main(int argc, char *argv[])
         for (int j = 0; j < n; j++)
             for (int k = 0; k < n; k++)
 #endif
-C[i * n + j] += A[i * n + k] * B[k * n + j];
+                C[i * n + j] += A[i * n + k] * B[k * n + j];
 #endif
 
+/*
+    BLAS macro is used to determine whether to use cblas library in computation and if so, then which function of library.
+    If BLAS is equal to 1 we will use cblas_sdot() function to compute matrix matrix product. For any other value of BLAS macro we will compute
+    matrix matrix product using cblas_sgemm() function.
+*/
 #ifdef BLAS
+
 #if BLAS == 1
-for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            C[i*n+j] = cblas_sdot(n,&A[i*n],1,&B[j*n],1);
-    std::cout<<"Using cblas_sdot function:"<<std::endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        /*
+            cblas_sdot computes: returns A . B ; here . represents dot product between vectors A and B
+
+            cblas_sdot syntax:
+            cblas_sdot(<number of elements in vecotrs passed>, <float * of first vector A>, 
+            <number to add in vector A to get next element>,
+            <float * of second vector B>, <number to add in vector B to get next element>)
+        */
+            C[i * n + j] = cblas_sdot(n, &A[i * n], 1, &B[j * n], 1);
+    std::cout << "Using cblas_sdot function:" << std::endl;
 #else
-    cblas_sgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,n,n,n,1,A,n,B,n,1,C,n);
-    std::cout<<"Using sgemm function:"<<std::endl;
+    /*
+        cblas_sgemm computes: C <- (alpha) * A * B + (beta) * C
+
+        cblas_sgemm syntax:
+            cblas_sgemm(<Layout of matrices passed>, <How matrix A should be used>,
+            <How matrix B should be used>, <First dimension length of matrix C and A>,
+            <Second dimension length of A and first dimension length of matrix B>, 
+            <Second dimension length of C and first dimension length of B>, <value of alpha>,
+            <float * of matrix A>, <leading dimensino of matrix A>, <float * of matrix B>, <leading dimension of matrix B>,
+            <vlaue of beta>, <float * of matrix C>, <leading dimension of matrix C>)
+    */
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1, A, n, B, n, 1, C, n);
+    std::cout << "Using sgemm function:" << std::endl;
 #endif
+
 #endif
+
 #endif
+
     const auto end = std::chrono::steady_clock::now();
     const std::chrono::duration<float> elapsedtime = end - start;
 
-    double computation_cost = 2.0*n*n*n;
+    /*
+        Computation cost for matrix matrix multiplication is 2N^3. Throguput = <Computation Cost> / <Time Taken>
+    */
+    double computation_cost = 2.0 * n * n * n;
     double flops = static_cast<double>(computation_cost / elapsedtime.count());
-    std::cout << "elapsed seconds: " << elapsedtime.count()<< " s" << std::endl;
-    std::cout<<"throughput: "<<flops<< " flops" <<std::endl;
+    std::cout << "elapsed seconds: " << elapsedtime.count() << " s" << std::endl;
+    std::cout << "throughput: " << flops << " flops" << std::endl;
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             for (int k = 0; k < n; k++)
+            /*
+                If BLAS = 1 then B is stored in Column Major layout
+            */
             #ifdef BLAS
             #if BLAS == 1
                 C_ref[i * n + k] += A[i * n + j] * B[k * n + j];
